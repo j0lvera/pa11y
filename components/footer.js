322e78681165bc3900b35f999257e96e65128710
j0lv3r4 <jolvera@posteo.de>
2019-04-21 15:54:33 -0500

    add license, modify readme.md

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,26 @@
 import { Box, Text, Link } from "@rebass/emotion";
+
+const links = [
+  //   {
+  //     label: "About",
+  //     link: "/about"
+  //   },
+  {
+    label: "GitHub",
+    link: "https://github.com/j0lv3r4/pa11y"
+  }
+];
+
 const Footer = () => (
-  <Box as="footer" py={4}>
+  <Box as="footer" py={4} width={[1, 1 / 3]} mx="auto">
     <Text textAlign="center" mb={0}>
-      A Weekend Project by <Link href="https://jolvera.biz">Juan Olvera</Link>
+      <p>
+        Made by <Link href="https://jolvera.biz">Juan Olvera</Link>
+      </p>
+
+      {links.map(({ label, link }) => (
+        <Link href={link}>{label}</Link>
+      ))}
     </Text>
   </Box>
 );
